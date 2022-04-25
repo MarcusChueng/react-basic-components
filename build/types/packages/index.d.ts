f393c343424c3d24c2e02d366814058f28fd5c1e
MarcusCheung <1285799157@qq.com>
2022-04-26 01:44:36 +0800

    feat: 首次发布

---
+++ build/types/packages/index.d.ts
@@ -0,0 +1,4 @@
+export type { TextViewProps } from "./TextView";
+export { default as TextView } from "./TextView";
+export type { Mp4PlayerProps } from "./MP4Player";
+export { default as MP4Player } from "./MP4Player";
--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@
  * @Author: MarcusCheung
  * @Date: 2022-04-25 18:45:14
  * @LastEditors: MarcusCheung
- * @LastEditTime: 2022-04-25 19:02:03
+ * @LastEditTime: 2022-04-25 23:21:41
  */
 import peerDepsExternal from "rollup-plugin-peer-deps-external"
 import resolve from "rollup-plugin-node-resolve"
@@ -13,7 +13,68 @@ import typescript from "rollup-plugin-typescript2"
 import postcss from "rollup-plugin-postcss"
 import json from "@rollup/plugin-json"
 
-const { babel } = require("rollup-plugin-babel")
+const { babel } = require("@rollup/plugin-babel")
 const packageJson = require("./package.json")
 const less = require("less")
 const isProd = process.env.NODE_ENV === "production"
+
+const babelOptions = {
+    presets: ["@babel/preset-env"],
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".less"],
+    exclude: "**/node_modules/**"
+}
+
+const processLess = function(context, payload) {
+    return new Promise((resolve, reject) => {
+        less.render(
+            {
+                file: context
+            },
+            function(err, result) {
+                if (!err) {
+                    resolve(result)
+                } else {
+                    reject(err)
+                }
+            }
+        )
+        less.render(context, {}).then(
+            function(output) {
+                if (output && output.css) {
+                    resolve(output.css)
+                } else {
+                    reject({})
+                }
+            },
+            function(err) {
+                reject(err)
+            }
+        )
+    })
+}
+
+export default {
+    input: "packages/index.ts",
+    output: [
+        {
+            file: packageJson.main,
+            format: "cjs"
+        },
+        {
+            file: packageJson.module,
+            format: "es"
+        }
+    ],
+    plugins: [
+        peerDepsExternal({ includeDependencies: !isProd }),
+        resolve(),
+        commonjs({ sourceMap: !isProd }),
+        typescript({ useTsconfigDeclarationDir: true }),
+        postcss({
+            extract: true,
+            process: processLess,
+        }),
+        babel(babelOptions),
+        json()
+    ]
+}
